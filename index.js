def465f3c595d0751c3d5ce408f4263ccf630aad
Soldy <4786022+Soldy@users.noreply.github.com>
2019-08-12 06:52:55 +0100

    joke remove
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const fs = require('fs');
 
 const cpuCalc = function(inp){
-    return (100-(                            
-         Math.floor(                         
-              (parseInt(inp[4])*100)/(       
-                  parseInt(inp[1])+          
-                  parseInt(inp[2])+          
-                  parseInt(inp[3])+          
-                  parseInt(inp[4])+           
-                  parseInt(inp[5])+           
-                  parseInt(inp[6])+           
-                  parseInt(inp[7])+           
-                  parseInt(inp[8])+           
-                  parseInt(inp[9])+           
-                  parseInt(inp[10])           
-             )                                
-        )                                     
-        ));
+    if(inp[1] == '')
+        inp.splice(1,1);
+    return (100-(
+         Math.floor(
+              (parseInt(inp[4])*100)/(
+                  parseInt(inp[1])+
+                  parseInt(inp[2])+
+                  parseInt(inp[3])+
+                  parseInt(inp[4])+
+                  parseInt(inp[5])+
+                  parseInt(inp[6])+
+                  parseInt(inp[7])+
+                  parseInt(inp[8])+
+                  parseInt(inp[9])+
+                  parseInt(inp[10])
+             )
+        )
+    ));
 }
+
+
 exports.stat={
     cpuTemp:function(){
         let cpuTemp = parseInt(fs.readFileSync('/sys/class/thermal/thermal_zone0/temp')),
@@ -32,39 +36,12 @@ exports.stat={
         readStat = readStat.toString().split("\n");
         for(let i = 0 ; readStat.length > i ; i++){
             readStat[i] = readStat[i].split(" ");
-            if(
-                (readStat[i][0] === 'cpu0')||
-                (readStat[i][0] === 'cpu1')||
-                (readStat[i][0] === 'cpu2')||
-                (readStat[i][0] === 'cpu3')||
-                (readStat[i][0] === 'cpu4')||
-                (readStat[i][0] === 'cpu5')||
-                (readStat[i][0] === 'cpu6')||
-                (readStat[i][0] === 'cpu7')||
-                (readStat[i][0] === 'cpu8')||
-                (readStat[i][0] === 'cpu9')||
-                (readStat[i][0] === 'cpu10')||
-                (readStat[i][0] === 'cpu11')||
-                (readStat[i][0] === 'cpu12')||
-                (readStat[i][0] === 'cpu13')||
-                (readStat[i][0] === 'cpu14')||
-                (readStat[i][0] === 'cpu15')||
-                (readStat[i][0] === 'cpu16')||
-                (readStat[i][0] === 'cpu17')||
-                (readStat[i][0] === 'cpu18')||
-                (readStat[i][0] === 'cpu19')||
-                (readStat[i][0] === 'cpu20')||
-                (readStat[i][0] === 'cpu21')||
-                (readStat[i][0] === 'cpu22')||
-                (readStat[i][0] === 'cpu23')||
-                (readStat[i][0] === 'cpu24')||
-                (readStat[i][0] === 'cpu25')||
-                (readStat[i][0] === 'cpu26')||
-                (readStat[i][0] === 'cpu27')||
-                (readStat[i][0] === 'cpu28')||
-                (readStat[i][0] === 'cpu29')
+            if (readStat[i][0] === 'cpu'){
+               out['all'] = calc(readStat[i]);
+            }else if(
+                (readStat[i][0].substring(0,3) === 'cpu')
             ){
-                out[readStat[i][0]] = cpuCalc(readStat[i]);
+                out[readStat[i][0]] = calc(readStat[i]);
             }
         }
 
